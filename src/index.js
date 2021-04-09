64fd987d6f77bbe5267a24953457b79a432aa5a6
Rahul Dubey <rahuld7219@gmail.com>
2021-04-09 17:40:49 +0530

    Declare a Winner

--- src/index.js
+++ src/index.js
@@ -31,6 +31,10 @@ class Board extends React.Component {
   handleClick(i) {
     const squares = this.state.squares.slice();
     //used slice to create a separate copy otherwise changes made to original one also.
+    if(calculateWinner(squares) || squares[i])
+    { //returns if someone has won the game or if a Square is already filled(not null)
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares, 
@@ -49,7 +53,16 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = `Next player:  ${this.state.xIsNext ? 'X' : 'O'}`;
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner)
+    {
+      status = "Winner: " + winner;
+    }
+    else
+    {
+     status = `Next player:  ${this.state.xIsNext ? 'X' : 'O'}`;
+    }
 
     return (
       <div>
@@ -96,3 +109,26 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares)//this is not a component(you can also see it by naming convention)
+{
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for(let i = 0; i < lines.length; i++)
+  {
+    const [a, b, c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c])//if squares[a]=>null then it equates to false
+    {
+      return squares[a];
+    }
+  }
+  return null;
+}
